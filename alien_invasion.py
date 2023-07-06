72961ea806e99f72283d208c08788b0589fc2f31
matsudatatsuya <matudatatuya7@gmail.com>
2023-07-06 09:17:00 +0900

    設定情報の追加

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,27 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("エイリアン侵略")
+
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
+
+            self.screen.fill(self.settings.bg_color)
+
             pygame.display.flip()
 
 
